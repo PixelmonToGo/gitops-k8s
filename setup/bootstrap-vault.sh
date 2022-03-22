d3d12286c8ee72774b013fed2ad1dd4c0162c58c
Chip Wolf ‮ <hello@chipwolf.uk>
2022-03-22 17:58:29 +0000

    fix(minecraft-dev): consolidate helm values

--- setup/bootstrap-vault.sh
+++ setup/bootstrap-vault.sh
@@ -181,9 +181,8 @@ loadSecretsToVault() {
   # helm chart values
   #################### 
   kvault "kube-system/oauth2-proxy/oauth2-proxy-helm-values.txt"
-  kvault "minecraft/minecraft/minecraft-forge-charmeleon-helm-values.txt"
-  kvault "minecraft/minecraft/minecraft-forge-squirtle-helm-values.txt"
-  kvault "minecraft-dev/minecraft/minecraft-forge-squirtle-helm-values.txt"
+  kvault "minecraft/minecraft/minecraft-forge-pikachu-helm-values.txt"
+  kvault "minecraft-dev/minecraft/minecraft-helm-values.txt"
   kvault "monitoring/datadog/datadog-helm-values.txt"
   kvault "monitoring/grafana/grafana-helm-values.txt"
   kvault "monitoring/kube-prometheus-stack/kube-prometheus-stack-helm-values.txt"
